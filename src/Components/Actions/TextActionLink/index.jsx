6cd3ff99deed3b78a8cb37fec672f649b22484a2
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-06-16 23:51:07 +0530

    style: 💄add links component

--- src/Components/Actions/TextActionLink/index.jsx
+++ src/Components/Actions/TextActionLink/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-const TextActionLink = () => {
-  return <div>TextActionLink</div>;
+const TextActionLink = ({ actionText = "", className = "", actionLink }) => {
+  return (
+    <NavLink to={actionLink} className={`${className} hover:text-500`}>
+      {actionText}
+    </NavLink>
+  );
 };
 
 export default TextActionLink;
