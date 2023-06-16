f092caba07276a28033122e14e68b0963c632355
Shraddha-V22 <shraddhav1935@gmail.com>
2023-06-16 18:21:05 +0530

    styles: 💄 buttons

--- src/Components/Actions/IconActionBtn/index.jsx
+++ src/Components/Actions/IconActionBtn/index.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
-const IconActionBtn = () => {
-  return <div>IconActionBtn</div>;
+const IconActionBtn = ({
+  actionText = "",
+  className = "",
+  actionType = "button",
+  actionHandler,
+}) => {
+  return (
+    <button
+      type={actionType}
+      onClick={actionHandler}
+      className={`${className} h-8 w-8 rounded-full p-1 text-sm capitalize text-800 duration-150 hover:bg-100 active:bg-200`}
+    >
+      {actionText}
+    </button>
+  );
 };
 
 export default IconActionBtn;
