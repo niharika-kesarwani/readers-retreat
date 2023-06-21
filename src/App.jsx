24a22b6973d2183901b15d8285ad00c771d3da30
Shraddha-V22 <shraddhav1935@gmail.com>
2023-06-21 17:50:23 +0530

    style: 💄 update secondary container

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import "./App.css";
 import SecondaryContainer from "./Components/Containers/SecondaryContainer";
 import Header from "./Layouts/Header";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+
 
 function App() {
   return (
     <div>
       <Header />
       <div className="flex h-[90vh] items-center justify-center gap-16">
-        <SecondaryContainer categoryName="student" />
-        <SecondaryContainer categoryName="book" />
+        <SecondaryContainer categoryName="student">
+          <div className="flex flex-col items-center gap-8">
+            <h1 className="text-2xl font-semibold uppercase">add student</h1>
+            <AddCircleIcon sx={{ fontSize: "50px" }} />
+          </div>
+        </SecondaryContainer>
+        <SecondaryContainer categoryName="book" >
+          <div className="flex flex-col items-center gap-8">
+            <h1 className="text-2xl font-semibold uppercase">add book</h1>
+            <AddCircleIcon sx={{ fontSize: "50px" }} />
+          </div>
+        </SecondaryContainer>
       </div>
     </div>
   );
--- src/Components/Containers/SecondaryContainer/index.jsx
+++ src/Components/Containers/SecondaryContainer/index.jsx
@@ -1,16 +1,11 @@
 import React from "react";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
 
-const SecondaryContainer = ({ categoryName, onClick }) => {
+const SecondaryContainer = ({children}) => {
   return (
     <section
-      onClick={onClick}
       className="grid h-[250px] w-[250px] cursor-pointer place-items-center rounded-md border hover:shadow-customBoxShadow"
     >
-      <div className="flex flex-col items-center gap-8">
-        <h1 className="text-2xl font-semibold uppercase">add {categoryName}</h1>
-        <AddCircleIcon sx={{ fontSize: "50px" }} />
-      </div>
+     {children}
     </section>
   );
 };
