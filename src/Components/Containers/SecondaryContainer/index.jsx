f78de88f7deb6ffeb954077b92304bd7fe9c569c
Shraddha Vkarma <97953020+Shraddha-V22@users.noreply.github.com>
2023-06-21 17:51:54 +0530

    sv/layout (#7)
    * style:💄 add secondary container
    
    * style: 💄 update secondary container

--- src/Components/Containers/SecondaryContainer/index.jsx
+++ src/Components/Containers/SecondaryContainer/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const SecondaryContainer = () => {
-  return <div></div>;
+const SecondaryContainer = ({children}) => {
+  return (
+    <section
+      className="grid h-[250px] w-[250px] cursor-pointer place-items-center rounded-md border hover:shadow-customBoxShadow"
+    >
+     {children}
+    </section>
+  );
 };
 
 export default SecondaryContainer;
