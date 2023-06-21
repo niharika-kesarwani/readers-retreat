7c726d89f5dafd983629a493c47cf450541fe4c0
Shraddha-V22 <shraddhav1935@gmail.com>
2023-06-21 17:39:57 +0530

    style:💄 add secondary container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import SecondaryContainer from "./Components/Containers/SecondaryContainer";
 import Header from "./Layouts/Header";
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="flex h-[90vh] items-center justify-center gap-16">
+        <SecondaryContainer categoryName="student" />
+        <SecondaryContainer categoryName="book" />
+      </div>
     </div>
   );
 }
