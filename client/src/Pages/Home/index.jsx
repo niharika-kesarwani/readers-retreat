50f747e29bd744559019ced40a4d0812872e1a0d
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-07-08 03:29:24 +0530

    feat: ✨ add pages

---
+++ client/src/Pages/Home/index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { SecondaryContainer } from "../../Components";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="flex h-[90vh] items-center justify-center gap-16">
+        <SecondaryContainer categoryName="student">
+          <div className="flex flex-col items-center gap-8">
+            <h1 className="text-2xl font-semibold uppercase">add student</h1>
+            <AddCircleIcon sx={{ fontSize: "50px" }} />
+          </div>
+        </SecondaryContainer>
+        <SecondaryContainer categoryName="book">
+          <div className="flex flex-col items-center gap-8">
+            <h1 className="text-2xl font-semibold uppercase">add book</h1>
+            <AddCircleIcon sx={{ fontSize: "50px" }} />
+          </div>
+        </SecondaryContainer>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
