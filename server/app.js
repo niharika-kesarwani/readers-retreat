3b3638e8e3e184dbf12fd29d19ccce29556106c2
Shraddha-V22 <shraddhav1935@gmail.com>
2023-07-09 08:49:02 +0530

    chore: connected to db

--- server/app.js
+++ server/app.js
@@ -1,8 +1,22 @@
+if (process.env.NODE_ENV !== "production") {
+  //for getting env variables
+  require("dotenv").config();
+}
+
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(process.env.MONGOOSE_CONNECTION_STR, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to db");
+});
 
 app.use(
   "/graphql",
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,65 +9,68 @@ const {
   GraphQLList,
 } = graphql;
 
-const books = [
-  {
-    id: "1",
-    title: "Eloquent JavaScript",
-    author: "Marijn Haverbeke",
-    description:
-      "This is a book about JavaScript, programming, and the wonders of the digital.",
-  },
-  {
-    id: "2",
-    title: "JavaScript: The Definitive Guide",
-    author: "David Flanagan",
-    description:
-      "JavaScript: The Definitive Guide is another beginner-friendly book for anyone interested in building powerful web apps.",
-  },
-];
-const lendingHistory = [
-  {
-    id: "1",
-    bookId: "1",
-    studentId: "1",
-    lentDate: "02/06/2023",
-    returnDate: "10/06/2023",
-  },
-  {
-    id: "2",
-    bookId: "2",
-    studentId: "2",
-    lentDate: "01/07/2023",
-    returnDate: "06/07/2023",
-  },
-  {
-    id: "3",
-    bookId: "1",
-    studentId: "3",
-    lentDate: "15/06/2023",
-    returnDate: "25/06/2023",
-  },
-];
-const students = [
-  {
-    id: "1",
-    name: "Niharika Kesarwani",
-    email: "niharikak@gmail.com",
-    phoneNo: "1234567890",
-  },
-  {
-    id: "2",
-    name: "Vivek Bhatt",
-    email: "vivekbhatt@gmail.com",
-    phoneNo: "1234567890",
-  },
-  {
-    id: "3",
-    name: "Shraddha Vishwakarma",
-    email: "shraddhav@gmail.com",
-    phoneNo: "1234567890",
-  },
-];
+// const books = [
+//   {
+//     id: "1",
+//     title: "Eloquent JavaScript",
+//     author: "Marijn Haverbeke",
+//     description:
+//       "This is a book about JavaScript, programming, and the wonders of the digital.",
+//   },
+//   {
+//     id: "2",
+//     title: "JavaScript: The Definitive Guide",
+//     author: "David Flanagan",
+//     description:
+//       "JavaScript: The Definitive Guide is another beginner-friendly book for anyone interested in building powerful web apps.",
+//   },
+// ];
+// const lendingHistory = [
+//   {
+//     id: "1",
+//     bookId: "1",
+//     studentId: "1",
+//     lentDate: "02/06/2023",
+//     returnDate: "10/06/2023",
+//   },
+//   {
+//     id: "2",
+//     bookId: "2",
+//     studentId: "2",
+//     lentDate: "01/07/2023",
+//     returnDate: "06/07/2023",
+//   },
+//   {
+//     id: "3",
+//     bookId: "1",
+//     studentId: "3",
+//     lentDate: "15/06/2023",
+//     returnDate: "25/06/2023",
+//   },
+// ];
+// const students = [
+//   {
+//     id: "1",
+//     rollNumber: 1,
+//     name: "Niharika Kesarwani",
+//     email: "niharikak@gmail.com",
+//     phoneNo: "1234567890",
+//   },
+//   {
+//     id: "2",
+//     rollNumber: 2,
+//     name: "Vivek Bhatt",
+//     email: "vivekbhatt@gmail.com",
+//     phoneNo: "1234567890",
+//   },
+//   {
+//     id: "3",
+//     rollNumber: 3,
+//     name: "Shraddha Vishwakarma",
+//     email: "shraddhav@gmail.com",
+//     phoneNo: "1234567890",
+//   },
+// ];
 
 const LendingHistoryType = new GraphQLObjectType({
   name: "LendingHistory",
@@ -76,15 +79,15 @@ const LendingHistoryType = new GraphQLObjectType({
     book: {
       type: BookType,
       resolve(parent, args) {
-        console.log(parent);
-        return _.find(books, { id: parent.bookId });
+        // console.log(parent);
+        // return _.find(books, { id: parent.bookId });
       },
     },
     student: {
       type: StudentType,
       resolve(parent, args) {
-        console.log(parent);
-        return _.find(students, { id: parent.studentId });
+        // console.log(parent);
+        // return _.find(students, { id: parent.studentId });
       },
     },
     lentDate: { type: GraphQLString },
@@ -102,7 +105,7 @@ const BookType = new GraphQLObjectType({
     lendingHistory: {
       type: new GraphQLList(LendingHistoryType),
       resolve(parent, args) {
-        return _.filter(lendingHistory, { bookId: parent.id });
+        // return _.filter(lendingHistory, { bookId: parent.id });
       },
     },
   }),
@@ -112,13 +115,14 @@ const StudentType = new GraphQLObjectType({
   name: "Student",
   fields: () => ({
     id: { type: GraphQLID },
+    rollNumber: { type: GraphQLString },
     name: { type: GraphQLString },
     email: { type: GraphQLString },
     phoneNo: { type: GraphQLString },
     lendingHistory: {
       type: new GraphQLList(LendingHistoryType),
       resolve(parent, args) {
-        return _.filter(lendingHistory, { studentId: parent.id });
+        // return _.filter(lendingHistory, { studentId: parent.id });
       },
     },
   }),
@@ -131,32 +135,32 @@ const RootQuery = new GraphQLObjectType({
       type: BookType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(books, { id: args.id });
+        // return _.find(books, { id: args.id });
       },
     },
     student: {
       type: StudentType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return _.find(students, { id: args.id });
+        // return _.find(students, { id: args.id });
       },
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return _.filter(books, {});
+        // return _.filter(books, {});
       },
     },
     students: {
       type: new GraphQLList(StudentType),
       resolve(parent, args) {
-        return _.filter(students, {});
+        // return _.filter(students, {});
       },
     },
     bookLendingHistory: {
       type: new GraphQLList(LendingHistoryType),
       resolve(parent, args) {
-        return _.filter(lendingHistory, {});
+        // return _.filter(lendingHistory, {});
       },
     },
   },
