6299aa13f2dadf1597190212b647b634b87d6be5
Niharika Kesarwani <45658446+niharika-kesarwani@users.noreply.github.com>
2023-07-15 13:11:50 +0530

    chore: add delete-update mutation functions (#21)

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -64,4 +64,81 @@ const addStudentMutation = gql`
   }
 `;
 
-export { getBooksQuery, getStudentsQuery, addBookMutation, addStudentMutation };
+const deleteBookMutation = gql`
+  mutation ($deleteBookId: ID!) {
+    deleteBook(id: $deleteBookId) {
+      id
+      name
+      author
+      description
+    }
+  }
+`;
+
+const deleteStudentMutation = gql`
+  mutation ($deleteStudentId: ID!) {
+    deleteStudent(id: $deleteStudentId) {
+      id
+      rollNumber
+      name
+      email
+      phoneNo
+    }
+  }
+`;
+
+const updateBookMutation = gql`
+  mutation (
+    $updateBookId: ID!
+    $updateBookName: String!
+    $updateBookAuthor: String!
+    $updateBookDescription: String!
+  ) {
+    updateBook(
+      id: $updateBookId
+      name: $updateBookName
+      author: $updateBookAuthor
+      description: $updateBookDescription
+    ) {
+      id
+      name
+      author
+      description
+    }
+  }
+`;
+
+const updateStudentMutation = gql`
+  mutation (
+    $updateStudentId: ID!
+    $updateStudentRollNo: String!
+    $updateStudentName: String!
+    $updateStudentEmail: String!
+    $updateStudentPhoneNo: String!
+  ) {
+    updateStudent(
+      id: $updateStudentId
+      rollNumber: $updateStudentRollNo
+      name: $updateStudentName
+      email: $updateStudentEmail
+      phoneNo: $updateStudentPhoneNo
+    ) {
+      id
+      rollNumber
+      name
+      email
+      phoneNo
+    }
+  }
+`;
+
+export {
+  getBooksQuery,
+  getStudentsQuery,
+  addBookMutation,
+  addStudentMutation,
+  deleteBookMutation,
+  deleteStudentMutation,
+  updateBookMutation,
+  updateStudentMutation,
+};
