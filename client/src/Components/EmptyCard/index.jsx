90dcde17627802408698f450f74e895382b2d267
Shraddha Vkarma <97953020+Shraddha-V22@users.noreply.github.com>
2023-07-15 22:48:32 +0530

    Update index.jsx

--- client/src/Components/EmptyCard/index.jsx
+++ client/src/Components/EmptyCard/index.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { ContainedActionBtn } from "../Actions";
 
-import { ContainedActionBtn } from "../Actions";
-import { useNavigate } from "react-router-dom";
-
 const EmptyCard = (props) => {
   const { navigate } = useNavigate();
   const { emptyCardUrl, emptyCardTitle, emptyCardDescription, addBtnText } =
