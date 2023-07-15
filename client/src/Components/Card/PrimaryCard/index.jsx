0f447e9a0136d02ed9369bd5dfb3d98aaad0f3ec
Vivek Bhatt <93856336+vivekbhatt07@users.noreply.github.com>
2023-07-15 08:15:41 +0530

    Vb/student (#15)
    * style: 💄 primary card
    
    * chore: ➕ add lottie dependency
    
    * style: 💄 primary card
    
    * style: 💄 primary card
    
    * style: 💄 primary card

--- client/src/Components/Card/PrimaryCard/index.jsx
+++ client/src/Components/Card/PrimaryCard/index.jsx
@@ -5,7 +5,7 @@ import { TruncUtil } from "../../../Utils";
 const PrimaryCard = (props) => {
   const { className, children } = props;
   const classes =
-    "primary-card rounded-lg p-4 bg-[#8EC5FC] relative overflow-hidden flex justify-center items-center " +
+    "primary-card rounded-lg p-4 bg-[#8EC5FC] relative overflow-hidden flex justify-center items-center min-w-[296px] max-w-[296px]" +
     className;
   return (
     <article className={classes}>
@@ -13,7 +13,10 @@ const PrimaryCard = (props) => {
       <span className="absolute left-0 top-0 flex h-[120px] w-[120px] -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-[#000] pl-10 pt-10 text-xl text-50">
         {props.rollNumber}
       </span>
-      <h3 className="text-xl">{TruncUtil(props.name, 12)}</h3>
+      <h3 className="text-xl capitalize">{TruncUtil(props.name, 12)}</h3>
+      <h3 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xl capitalize text-[#fff]">
+        {TruncUtil(props.name, 12)}
+      </h3>
     </article>
   );
 };
