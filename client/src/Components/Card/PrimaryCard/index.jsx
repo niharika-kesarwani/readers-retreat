6d0929e590e872c714134a143cc3faca664aa27d
Vivek Bhatt <vivekbhatt618@gmail.com>
2023-07-15 07:04:08 +0530

    style: 💄 primary card

---
+++ client/src/Components/Card/PrimaryCard/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./PrimaryCard.css";
+import { TruncUtil } from "../../../Utils";
+
+const PrimaryCard = (props) => {
+  const { className, children } = props;
+  const classes =
+    "primary-card rounded-lg p-4 bg-[#8EC5FC] relative overflow-hidden flex justify-center items-center " +
+    className;
+  return (
+    <article className={classes}>
+      <span className="primary-card-index absolute left-0 top-0 flex h-[120px] w-[120px] -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-[#000]"></span>
+      <span className="absolute left-0 top-0 flex h-[120px] w-[120px] -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-[#000] pl-10 pt-10 text-xl text-50">
+        {props.id}
+      </span>
+      <h3 className="text-xl">{TruncUtil(props.studentName, 12)}</h3>
+    </article>
+  );
+};
+
+export default PrimaryCard;
--- client/src/Pages/Student/index.jsx
+++ client/src/Pages/Student/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./Student.css";
+import PageContainer from "../../Layouts/PageContainer";
+import { PrimaryCard } from "../../Components";
 
 const Student = () => {
-  return <div></div>;
+  const studentData = [
+    { id: "1", studentName: "Vivek Bhatt", bgColor: "#8EC5FC" },
+    { id: "2", studentName: "Niharika Kesarwani", bgColor: "#E0C3FC" },
+    { id: "3", studentName: "Pranita Fulsundar", bgColor: "#80D0C7" },
+    { id: "4", studentName: "Abhishek Gupta", bgColor: "#8EC5FC" },
+    { id: "5", studentName: "Shraddha Vishwakarma", bgColor: "#8EC5FC" },
+  ];
+  return (
+    <PageContainer className="flex">
+      <div className="flex w-full flex-col gap-8 px-4 py-4 md:flex-row xl:px-0">
+        <ul className="card-container w-full">
+          {studentData.map((currentStudent) => {
+            return <PrimaryCard key={currentStudent.id} {...currentStudent} />;
+          })}
+        </ul>
+      </div>
+    </PageContainer>
+  );
 };
 
 export default Student;
