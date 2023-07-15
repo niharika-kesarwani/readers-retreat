f672abf493e6fc02c6c848cfbca87cc6ee6babbc
Shraddha Vkarma <97953020+Shraddha-V22@users.noreply.github.com>
2023-07-15 19:44:13 +0530

    feat: 🔍add search students/books (#27)

--- client/src/Pages/Book/index.jsx
+++ client/src/Pages/Book/index.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { graphql } from "@apollo/client/react/hoc";
 import { getBooksQuery } from "../../queries/queries";
 import "./Book.css";
 import PageContainer from "../../Layouts/PageContainer";
 import { PrimaryCard, EmptyCard } from "../../Components";
+import InputSearch from "../../Components/Inputs/InputSearch";
 
 const Book = () => {
   const { data, loading, error } = useQuery(getBooksQuery);
+  const [inputText, setInputText] = useState("");
+
+  const booksData = useMemo(() => {
+    if (inputText) {
+      return data?.books.filter(
+        (book) =>
+          book.name.toLowerCase().includes(inputText.toLowerCase()) ||
+          book.author.toLowerCase().includes(inputText.toLowerCase()) ||
+          book.description.toLowerCase().includes(inputText.toLowerCase())
+      );
+    } else {
+      return data?.books;
+    }
+  }, [data, inputText]);
 
   if (loading) {
     return <div>Loading books... </div>;
@@ -17,17 +32,27 @@ const Book = () => {
     return <div>Something went wrong!</div>;
   }
   return (
-    <PageContainer className="flex">
+    <PageContainer className="m-4 flex flex-col items-center gap-4">
+      <InputSearch searchName="Books" setInputText={setInputText} />
       <div className="flex w-full flex-col gap-8 px-4 py-4 md:flex-row xl:px-0">
         <ul
           className={
-            data.books.length == 0 ? `flex w-full` : `card-container w-full`
+            data.books.length == 0
+              ? `flex w-full`
+              : `card-container w-full justify-items-center`
           }
         >
           {data.books.length !== 0 ? (
-            data.books.map((currentBook) => {
-              return <PrimaryCard key={currentBook.id} {...currentBook} />;
-            })
+            booksData.length ? (
+              booksData.map((currentBook) => {
+                return <PrimaryCard key={currentBook.id} {...currentBook} />;
+              })
+            ) : (
+              <p className="text-xl">
+                No Book with title/author/description of{" "}
+                <strong>{inputText}</strong> found
+              </p>
+            )
           ) : (
             <EmptyCard
               emptyCardTitle="bookEmpty"
