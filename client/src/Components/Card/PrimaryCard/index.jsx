ea4fd5311c8beb036d45b503877b3bec80121801
Vivek Bhatt <93856336+vivekbhatt07@users.noreply.github.com>
2023-07-15 09:19:08 +0530

    Vb/student (#18)
    * style: 💄 primary card
    
    * chore: ➕ add lottie dependency
    
    * style: 💄 primary card
    
    * style: 💄 primary card
    
    * style: 💄 primary card
    
    * style: 💄 Student Detail Modal

--- client/src/Components/Card/PrimaryCard/index.jsx
+++ client/src/Components/Card/PrimaryCard/index.jsx
@@ -1,23 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PrimaryCard.css";
 import { TruncUtil } from "../../../Utils";
+import ModalProvider from "../../ModalProvider";
 
 const PrimaryCard = (props) => {
   const { className, children } = props;
+  const [isModalOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+
   const classes =
-    "primary-card rounded-lg p-4 bg-[#8EC5FC] relative overflow-hidden flex justify-center items-center min-w-[296px] max-w-[296px]" +
+    "primary-card rounded-lg p-4 bg-[#8EC5FC] relative overflow-hidden flex justify-center items-center min-w-[296px] max-w-[296px] h-full cursor-pointer " +
     className;
   return (
-    <article className={classes}>
-      <span className="primary-card-index absolute left-0 top-0 flex h-[120px] w-[120px] -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-[#000]"></span>
-      <span className="absolute left-0 top-0 flex h-[120px] w-[120px] -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-[#000] pl-10 pt-10 text-xl text-50">
-        {props.rollNumber}
-      </span>
-      <h3 className="text-xl capitalize">{TruncUtil(props.name, 12)}</h3>
-      <h3 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xl capitalize text-[#fff]">
-        {TruncUtil(props.name, 12)}
-      </h3>
-    </article>
+    <ModalProvider
+      isOpen={isModalOpen}
+      closeModal={closeModal}
+      modalTitle="STUDENT DETAIL"
+      modalBtnVariant={
+        <article className={classes} onClick={() => openModal()}>
+          <span className="primary-card-index absolute left-0 top-0 flex h-[120px] w-[120px] -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-[#000]"></span>
+          <span className="absolute left-0 top-0 flex h-[120px] w-[120px] -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-[#000] pl-10 pt-10 text-xl text-50">
+            {props.rollNumber}
+          </span>
+          <h3 className="text-xl capitalize">{TruncUtil(props.name, 12)}</h3>
+          <h3 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-xl capitalize text-[#fff]">
+            {TruncUtil(props.name, 12)}
+          </h3>
+        </article>
+      }
+    >
+      <div className="flex flex-col gap-3 p-4">
+        <div className="flex justify-between rounded-md bg-100 p-2">
+          <span>ID</span>
+          <span className="capitalize">{props.rollNumber}</span>
+        </div>
+        <div className="flex justify-between rounded-md bg-100 p-2">
+          <span>Name</span>
+          <span className="capitalize">{props.name}</span>
+        </div>
+        <div className="flex justify-between rounded-md bg-100 p-2">
+          <span>Email</span>
+          <span className="capitalize">{props.email}</span>
+        </div>
+        <div className="flex justify-between rounded-md bg-100 p-2">
+          <span>Phone Number</span>
+          <span className="capitalize">{props.phoneNo}</span>
+        </div>
+      </div>
+    </ModalProvider>
   );
 };
 
