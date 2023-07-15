5a980dd9cdb3f8373f1cdd31613a723290f88883
Shraddha-V22 <shraddhav1935@gmail.com>
2023-07-15 19:42:47 +0530

    feat: 🔍add search students/books

--- client/src/Pages/Student/index.jsx
+++ client/src/Pages/Student/index.jsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { graphql } from "@apollo/client/react/hoc";
 import { getStudentsQuery } from "../../queries/queries";
 import "./Student.css";
 import PageContainer from "../../Layouts/PageContainer";
 import { PrimaryCard, EmptyCard } from "../../Components";
+import InputSearch from "../../Components/Inputs/InputSearch";
 
 const Student = () => {
   const { data, loading, error } = useQuery(getStudentsQuery);
+  const [inputText, setInputText] = useState("");
+
+  const studentsData = useMemo(() => {
+    if (inputText) {
+      return data?.students.filter(
+        (student) =>
+          student.name.toLowerCase().includes(inputText.toLowerCase()) ||
+          student.email.toLowerCase().includes(inputText.toLowerCase())
+      );
+    } else {
+      return data?.students;
+    }
+  }, [data, inputText]);
 
   if (loading) {
     return <div>Loading students... </div>;
@@ -17,23 +31,32 @@ const Student = () => {
     return <div>Something went wrong!</div>;
   }
   return (
-    <PageContainer className="flex">
+    <PageContainer className="m-4 flex flex-col items-center gap-4">
+      <InputSearch searchName="Students" setInputText={setInputText} />
       <div className="flex w-full flex-col gap-8 px-4 py-4 md:flex-row xl:px-0">
         <ul
           className={
-            data.students.length == 0 ? `flex w-full` : `card-container w-full`
+            data.students.length == 0
+              ? `flex w-full`
+              : `card-container w-full justify-items-center`
           }
         >
           {data.students.length !== 0 ? (
-            data.students.map((currentStudent) => {
-              return (
-                <PrimaryCard
-                  key={currentStudent.id}
-                  {...currentStudent}
-                  isStudent
-                />
-              );
-            })
+            studentsData.length ? (
+              studentsData.map((currentStudent) => {
+                return (
+                  <PrimaryCard
+                    key={currentStudent.id}
+                    {...currentStudent}
+                    isStudent
+                  />
+                );
+              })
+            ) : (
+              <p className="text-xl">
+                No Student with name/email of <strong>{inputText}</strong> found
+              </p>
+            )
           ) : (
             <EmptyCard
               emptyCardTitle="studentEmpty"
